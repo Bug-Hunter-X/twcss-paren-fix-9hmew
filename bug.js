a5adc31ddc02cd02e31003e61705b946829b3a6f
BugHunter-X <askzenai@gmail.com>
2024-12-05 01:28:07 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,11 @@
+```javascript
+<div class="bg-gray-100 p-4">
+  <div class="flex items-center space-x-4">
+    <div class="w-12 h-12 bg-gray-300 rounded-full"></div>
+    <div>
+      <p class="text-lg font-medium">Error: Unmatched parentheses</p>
+      <p class="text-gray-600">This error typically occurs when your Tailwind CSS classes are not properly formatted or contain syntax errors.</p>
+    </div>
+  </div>
+</div>
+```
